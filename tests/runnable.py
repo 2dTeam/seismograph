d51de0a465e7d312bbbc77fa8285f2a402a90e23
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2016-11-22 19:11:39 +0300

    so all test must be named with test_ prefix

--- tests/runnable.py
+++ tests/runnable.py
@@ -3,7 +3,7 @@ import seismograph.runnable as runnable
 
 
 class TestRunnableGroup(unittest.TestCase):
-    def make_instance(self):
+    def test_make_instance(self):
         mock_config = {"mock_var": 1}
         mock_object = ["some_list"]
         run = runnable.RunnableGroup(mock_object, mock_config)

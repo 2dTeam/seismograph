c7323c61b5a80b2271318c70779687745719561f
Paul Maminov <xobotun@inbox.ru>
2016-11-22 15:29:12 +0300

    Added one more test.

--- tests/scope.py
+++ tests/scope.py
@@ -14,4 +14,11 @@ class TestCaseContext(BaseTestCase):
         pass
 
     def test_scope__add_extension__raises_on_wrong_file(self):
-        with self.assertRaises(NotImplementedError) as error: scope.add_extension("wrong_extension")
+        with self.assertRaises(NotImplementedError): scope.add_extension("wrong_extension")
+
+    def test_scope__add_extension__finds_install(self):
+        import seismograph.ext.mocker as mockedExtension
+        try:
+            scope.add_extension(mockedExtension)
+        except NotImplementedError:
+            self.fail("myFunc() raised NotImplementedError unexpectedly!")

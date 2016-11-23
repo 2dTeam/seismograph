539d6a9ff8487b3aad68aeac4cb5c226dd3d8618
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2016-11-24 02:42:58 +0300

    Tests added

--- tests/runnable.py
+++ tests/runnable.py
@@ -2,12 +2,49 @@ import unittest
 import seismograph.runnable as runnable
 
 
+class TestRunnableObject(unittest.TestCase):
+
+    class InnerRunnable(runnable.RunnableObject):
+
+        def __init__(self):
+            self._mock_var = False
+
+        def __run__(self, *args, **kwargs):
+            self.mock_var = True
+
+        @property
+        def mock_var(self):
+            return self._mock_var
+
+        @mock_var.setter
+        def mock_var(self, value):
+            self._mock_var = value
+
+    def setUp(self):
+        self.mock_arr = [1, 2, 3]
+        self.mock_kwarg = {"test": "mock"}
+        self.instance = self.InnerRunnable()
+
+    def test_run(self):
+        runnable.run(self.instance, self.mock_arr, self.mock_kwarg)
+        self.assertEqual(self.instance.mock_var, True)
+
+
 class TestRunnableGroup(unittest.TestCase):
+    def setUp(self):
+        self.mock_config = {"mock_var": 1}
+        self.mock_object = ["some_list"]
+
     def test_make_instance(self):
-        mock_config = {"mock_var": 1}
-        mock_object = ["some_list"]
-        run = runnable.RunnableGroup(mock_object, mock_config)
-        self.assertEqual(run.config, mock_config)
+        run = runnable.RunnableGroup(self.mock_object, self.mock_config)
+        self.assertEqual(run.config, self.mock_config)
+        self.assertEqual(run.objects, self.mock_object)
+
+# class TestFuncRunnable(unittest.TestCase):
+#     def test_run_method(self):
+
+
+
 
 if __name__ == '__main__':
     unittest.main()

5309a81a35d0011cf53af9dc7323889b16e17d0e
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2016-11-26 21:26:55 +0300

    test added

--- tests/script.py
+++ tests/script.py
@@ -4,17 +4,22 @@ from seismograph import script
 
 
 class MockShouldStopClass:
-    def __init__(self):
-        self.should_stop = False
+    def __init__(self, should_stop=False):
+        self._should_stop = should_stop
 
+    @property
     def should_stop(self):
-        return self.should_stop
+        return self._should_stop
+
+    @should_stop.setter
+    def should_stop(self, value):
+        self._should_stop = value
 
 
 class MockClassScript:
-    def __init__(self):
+    def __init__(self, should_stop=False):
         self._that_self = None
-        self._mock_var = MockShouldStopClass()
+        self._mock_var = MockShouldStopClass(should_stop=should_stop)
 
     @property
     def current_state(self):
@@ -32,14 +37,20 @@ class MockClassScript:
         self._that_self = value
 
 
+
+
 class MockClassProgram:
     class MockClassProgramConfig:
         def __init__(self):
-            self.STOP = False
+            self._STOP = False
 
         @property
         def STOP(self):
-            return self.STOP
+            return self._STOP
+
+        @STOP.setter
+        def STOP(self, value):
+            self._STOP = value
 
     def __init__(self):
         self._config = self.MockClassProgramConfig()
@@ -54,11 +65,16 @@ class TestScriptRun(unittest.TestCase):
         self.program = MockClassProgram()
         self.methodName = "MethodName"
 
-    def test_run(self):
+    def test_run_raises_debug_error(self):
         script_obj = script.Script(self.program, self.methodName)
-        inner_instance = MockClassScript()
+        inner_instance = MockClassScript(should_stop=False)
         with self.assertRaises(AttributeError): script_obj.__run__(inner_instance)
 
+    def test_run_with_should_stop(self):
+        script_obj = script.Script(self.program, self.methodName)
+        inner_instance = MockClassScript(should_stop=True)
+        script_obj.__run__(inner_instance)
+        self.assertEqual(inner_instance.current_state.should_stop, True)
 
 if __name__ == '__main__':
     unittest.main()
